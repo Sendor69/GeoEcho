ca34f02df05a124f634b64c2f4427ff4f96fdd90
DaniP <danisdr@hotmail.es>
2018-03-03 01:46:28 +0100

    Initial commit

---
+++ app/src/main/java/papaya/geoecho/LogReg.java
@@ -0,0 +1,48 @@
+package papaya.geoecho;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static papaya.geoecho.R.id.rLogin;
+import static papaya.geoecho.R.id.rRegister;
+
+public class LogReg extends AppCompatActivity implements View.OnClickListener {
+
+    //UI components
+    Button Login, Sign;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_reg);
+
+        Login = (Button) findViewById(rLogin);
+        Login.setOnClickListener(this);
+        Sign = (Button) findViewById(R.id.rRegister);
+        Sign.setOnClickListener(this);
+
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case rLogin:
+                Intent i = new Intent(this, Login.class);
+                startActivity(i);
+                break;
+            case rRegister:
+                Intent intent = new Intent(this, Registre.class);
+                startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/papaya/geoecho/Login.java
@@ -0,0 +1,92 @@
+package papaya.geoecho;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static papaya.geoecho.R.id.bLogin;
+import static papaya.geoecho.R.id.tRecordar;
+
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    //Referencias UI
+
+    Button login;
+    EditText user,password;
+    TextView record;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        login = (Button) findViewById(bLogin);
+        login.setOnClickListener(this);
+        user = (EditText)findViewById(R.id.eUsername);
+        password = (EditText) findViewById(R.id.ePasword);
+        record = (TextView) findViewById(tRecordar);
+        record.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick (View view) {
+        switch (view.getId()){
+            case bLogin:
+                new UserLoginTask().execute();
+                break;
+            case tRecordar:
+                //ToDO por implementar
+                break;
+        }
+
+    }
+
+
+
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        /*Clase tonta, no hace nada */
+
+        ProgressDialog mDialog = new ProgressDialog(Login.this);
+
+        UserLoginTask() {
+
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: intenta conectarte al server y logear.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mDialog.dismiss();
+
+        }
+        @Override
+        protected void onPreExecute() {
+            //Autenticando login...
+            mDialog.setMessage("Authenticating...");
+            mDialog.show();
+        }
+    }
+}
